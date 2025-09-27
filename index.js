4d09f63575803663db061ff9bd327d0d64f768cf
Saurav Gaur <saurav@webnexus.in>
2025-09-27 12:05:57 +0530

    your commit message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // A) using var
 for (var i = 0; i < 3; i++) {
-  console.log("changed");
+  console.log("chanes for the feature branch");
   for (let o = 0; o < 5; o++) {
     console.log("changed again new git changes");
   }
